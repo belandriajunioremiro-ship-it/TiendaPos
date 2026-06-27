e9dcca5797284f72f3556e034d21291d60b37c23
BelandriaJuniorEmiro <belandriajunioremiro@gmail.com>
2026-06-27 12:25:54 -0400

    fix: actualizada URL a https://tiendapos-app.vercel.app para SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: "https://tiendapos.com",
+  site: "https://tiendapos-app.vercel.app",
   integrations: [react(), mdx(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
